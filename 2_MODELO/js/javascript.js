224395dac76e6d4ffe457b6833a8bca48b42102e
Diego Ripper <dcrmendes26@gmail.com>
2022-08-23 17:23:43 -0300

    inicio javascript

--- 2_MODELO/js/javascript.js
+++ 2_MODELO/js/javascript.js
@@ -7,8 +7,22 @@ var b = 33;
 
 alert (a + b); */
 
-function somar(a, b){
+const a = 10;
+const b = 33;
+
+
+function somar(a, b) {
    return a + b;
 }
 
-console.log(somar(2, 5))
+/* console.log(somar(2, 5)) */
+
+document.querySelector("#calcular").addEventListener("click", function () {
+   let valorA = document.querySelector("#valorA").value;
+   let valorB = document.querySelector("#valorB").value;
+   if (valorA.lenght > 0 || valorB.lenght > 0) {
+      alert(parseInt(valorA) + parseInt(valorB));
+   } else {
+      alert("Digite um número");
+   };
+})
