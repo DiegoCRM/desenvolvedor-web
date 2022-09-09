3f6b160d9605a887cc4434f664cdc6c3681e1ecd
Diego Ripper <dcrmendes26@gmail.com>
2022-09-09 16:36:42 -0300

    ajustando canvas mobile

--- 3_PROJETO/js/efeitos.js
+++ 3_PROJETO/js/efeitos.js
@@ -49,6 +49,20 @@ $(document).ready(function () {
         event.preventDefault();
     })
 
+    $("#btn-bars").on("click", function(){
+ 
+        $("header").toggleClass("open-menu");
+   
+      });
+   
+      $("#menu-mobile-mask, .btn-close").on("click", function(){
+   
+        $("header").removeClass("open-menu");
+   
+   
+      });
+  
+
 });
 
 
